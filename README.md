3be566211dadbecd21605796dbef1ab3c2674b24
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2024-05-09 15:06:40 +0200

    feat: add letLazy

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ TODO
 
 -   [x] strings: take, drop, head, last, tail, reverse
 -   [ ] numbers:
+-   [ ] property access using path 'a.b.c[0]' get(path, defaultVal) has()
+-   [ ] set operations
 -   [ ] objects: eq, copy("attributes.A"=44),
 -   [ ] use typescript
+-   [ ] add wrappers: Option, Either, Try
 -   [ ] use macros
--- example.js
+++ example.js
@@ -1,6 +1,67 @@
 import { camelCase, chunk, kebabCase, snakeCase } from './index.js'
+import { letLazy } from './lazy.js'
 
 const text = 'foo bar'
 console.log(text, text[camelCase](), text[kebabCase](), text[snakeCase]())
 
 console.log(text[chunk](3))
+
+const add = () => {}
+const multiply = () => {}
+const divide = () => {}
+
+const pipe =
+    (...fns) =>
+    (x) =>
+        fns.reduce((v, f) => f(v), x)
+
+//add(44)[pipe](multiply, multiply, divide)[pipe](divide)
+
+//
+// -----------
+//
+
+function sleep(seconds) {
+    var e = new Date().getTime() + seconds * 1000
+    while (new Date().getTime() <= e) {}
+}
+
+letLazy('hamsti', () => {
+    sleep(3)
+    console.log('####')
+    return 'meowww'
+})
+
+class Soolong {
+    moolong = 40
+
+    constructor() {
+        letLazy(
+            'moo',
+            () => {
+                sleep(3)
+                return ++this.moolong
+            },
+            this
+        )
+    }
+
+    koo() {
+        console.log('## ', this.moolong)
+        return this.moo
+    }
+}
+
+const s = new Soolong()
+
+console.log('--->1')
+console.log('-', s.koo())
+console.log('--->2')
+console.log('-', s.koo())
+console.log('--->3')
+
+console.log('--->1')
+console.log('-', hamsti)
+console.log('--->2')
+console.log('-', hamsti)
+console.log('--->3')
